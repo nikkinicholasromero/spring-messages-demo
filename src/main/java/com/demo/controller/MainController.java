f29a3daa441fdf34450cb3dbfe6e6da8d44d08ed
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2020-07-13 22:37:36 -0400

    Added MessageSource

--- src/main/java/com/demo/controller/MainController.java
+++ src/main/java/com/demo/controller/MainController.java
@@ -1,12 +1,19 @@
 package com.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class MainController {
+    @Autowired
+    private MessageSource messageSource;
+
     @GetMapping("")
     public String main() {
-        return "Hello, World";
+        return messageSource.getMessage("greetings", null, Locale.US);
     }
 }
